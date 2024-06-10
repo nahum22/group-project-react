0fa862c6f2ae51ccf2374aec290f4ac35a4c427e
Artur Abel <163734294+ArturAbel@users.noreply.github.com>
2024-06-10 11:38:45 +0300

    fetch cars added

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,16 +1,40 @@
-
-import { createContext, useState, useContext } from "react";
+import axios from "axios";
+import { createContext, useState, useContext, useEffect } from "react";
+const url = "https://6666aa30a2f8516ff7a44b9d.mockapi.io/cars";
 
 const AppContext = createContext();
-const url="https://6666aa30a2f8516ff7a44b9d.mockapi.io/cars"
+
 export const AppProvider = ({ children }) => {
-  //define usestate here
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [carsData, setCarsData] = useState([]);
+
+  useEffect(() => {
+    const fetchCars = async () => {
+      try {
+        const response = await axios.get(url);
+        setCarsData(response.data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCars();
+  }, [url]);
+
+
 
   //todo: create, read, update
 
   //we need to put thier names in the value
 
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ carsData, error, loading }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
