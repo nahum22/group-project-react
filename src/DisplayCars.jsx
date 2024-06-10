11cd0c98bf1db3da96b6d855ac5fa1eda915f802
nahum22 <nahum.nahum@gmail.com>
2024-06-10 10:37:59 +0300

    tamplate

---
+++ src/DisplayCars.jsx
@@ -0,0 +1,10 @@
+import { AppProvider } from "./context";
+import React from "react";
+
+const AppContext = createContext();
+
+const DisplayCars = () => {
+  return <div>DisplayCars</div>;
+};
+
+export default DisplayCars;
