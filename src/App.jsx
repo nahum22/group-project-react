abc359102c0fcf5ec84c97892fc1aedcb7237226
nahum22 <nahum.nahum@gmail.com>
2024-06-10 10:54:43 +0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Login from "./Login";
+
 function App() {
-  return <></>;
+  return <Login />;
 }
 
 export default App;
--- src/Update.jsx
+++ src/Update.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useGlobalContext } from "./context";
+import { useGlobalContext } from "./Context";
 const Update = () => {
   // between the brackets we need to write the field  to import
   // const {  } = useGlobalContext();
